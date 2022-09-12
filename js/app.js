c8727cadbe9f34d7a1faf4cab308dfa03e0dfd43
Sebastian Sanchez <sebastian.asm@outlook.com>
2022-09-11 23:58:10 -0300

    mejoramiento de clases css y avances en js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,42 @@
-const select = document.querySelector('select');
+const d = document;
+const selectTag = d.querySelector('#select-tag');
+const templateCards = d.querySelector('#template-cards').content;
+const fragment = d.createDocumentFragment();
+const contentCards = d.querySelector('#content-cards');
 
-document.addEventListener('DOMContentLoaded', app);
+d.addEventListener('DOMContentLoaded', app);
 
 function app() {
-  createCards(select.value);
-  select.addEventListener('change', ({ target }) => createCards(target.value));
+  d.querySelector('#year').textContent = new Date().getFullYear();
+  createCards(selectTag.value);
+  selectTag.addEventListener('change', ({ target }) =>
+    createCards(target.value)
+  );
 }
 
 async function createCards(optionValue) {
   const resp = await fetch('./assets/db/db.json');
   const data = await resp.json();
   const items = data.filter((item) => item.tag === optionValue);
-  console.log(items);
+
+  contentCards.innerHTML = '';
+  if (items.length > 0) {
+    items.forEach(uiCard);
+  } else {
+    console.log('no hay elementos');
+  }
+}
+
+function uiCard(item) {
+  const { title, description, image, url } = item;
+
+  templateCards.querySelector('a').href = url;
+  templateCards.querySelector('img').src = image;
+  templateCards.querySelector('img').alt = title;
+  templateCards.querySelector('h2').textContent = title;
+  templateCards.querySelector('p').textContent = description;
+
+  const clone = templateCards.cloneNode(true);
+  fragment.appendChild(clone);
+  contentCards.appendChild(fragment);
 }
